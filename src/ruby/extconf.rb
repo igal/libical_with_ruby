4ad9e10859936a759b060f58ad8ee68399314a2f
Igal Koshevoy <igal@pragmaticraft.com>
2008-11-16 18:30:10 -0800

    Added hack to compile external .o files into the .so, thus making the library usable.

--- src/ruby/extconf.rb
+++ src/ruby/extconf.rb
@@ -1,4 +1,7 @@
 require 'mkmf'
 dir_config('LibicalWrap')
+# FIXME What's the right way to pass INCFLAGS?
 $INCFLAGS << ' -I.. -I../libical -I../libicalss'
+# FIXME What's the right way to tell it to include extra .o files?
+CONFIG['LDSHARED'] << ' ' << Dir['../libical/*.o', '../libicalss/*.o', 'LibicalWrap.o'].join(' ')
 create_makefile('LibicalWrap')
