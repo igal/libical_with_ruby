b06a07391620e12753bfef37394ec9a3b0ce9ad0
Igal Koshevoy <igal@pragmaticraft.com>
2008-11-16 23:52:38 -0800

    Added Ruby-based example, although it doesn't work because the recommended icalparser_parse_string function doesn't work even within C code.

---
+++ src/ruby/example.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+# SUMMARY: Simplest possible use of the paring library
+
+require 'LibicalWrap'
+
+# From http://en.wikipedia.org/wiki/ICalendar
+ics = <<-HERE
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+BEGIN:VEVENT
+DTSTART:19970714T170000Z
+DTEND:19970715T035959Z
+SUMMARY:Bastille Day Party
+END:VEVENT
+END:VCALENDAR
+HERE
+
+ics = File.read('../../test-data/2445.ics')
+
+# FIXME Make parser return a calender, not a void
+calendar = LibicalWrap.icalparser_parse_string(ics)
+puts "Parsed ics data and got: #{calendar.inspect}"
